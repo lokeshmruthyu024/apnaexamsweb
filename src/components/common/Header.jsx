fa220bcfa4bd63276acf5a675a3b0d4cd8dd54ae
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2025-11-19 12:42:08 +0530

    complete header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import Logo from '../../assets/Logo.png'
+import { FaBlog, FaHome, FaUser } from 'react-icons/fa'
+import { TbFiles } from 'react-icons/tb'
+import { FaPeopleLine } from 'react-icons/fa6'
+
 const Header = () => {
   return (
     <div className='container'>
       <div className="row my-1">
-        <div className="col-md-2">
+        <div className="col-md-2 ">
           <img src={Logo} alt="" />
         </div>
-        <div className="col-md-6">
-          <ul className='d-flex justify-content-between align-items-center my-4 list-unstyled'>
-            <li className=''>🏡Home</li>
-            <li>👤About us</li>
-            <li>📄Exam Packages</li>
-            <li>📜Live Exams</li>
-            <li>🤼Services</li>
-            <li>🌏Blog</li>
+        <div className="col-md-7">
+          <ul className='nav-class'>
+            <li><a href=""><FaHome />Home</a></li>
+            <li><a href="" ><FaUser />About Us</a></li>
+            <li><a href="" ><TbFiles />Exam packages</a></li>
+            <li><a href="" ><FaPeopleLine />Live Exams</a></li>
+            <li><a href="" ><FaHome />Services</a></li>
+            <li><a href=""><FaBlog />Blogs</a></li>
           </ul>
         </div>
-        {/* <div className="col-md-2">
-        </div> */}
-        <div className="col-md-4">
-          <div className="d-flex">
-            <button className='text-white p-2 rounded-2 my-3 border-0 fw-bold' style={{ backgroundColor: '#2abfa4' }}>👤 Member Login</button>
-            <div className='my-4 mx-1 p-1 rounded' style={{ backgroundColor: '#004f9e' }}>📞</div>
-            <div className='text-white my-4 p-1 rounded' style={{ backgroundColor: '#004f9e' }}>+91 8886100031</div>
+        <div className="col-md-3 my-4">
+          <div className="header-buttons">
+            <button className="login-btn">Member Login</button>
+            <button className="phone-btn ">
+              <span className="me-1">📞</span>
+              +91 8886100031
+            </button>
           </div>
         </div>
       </div>
