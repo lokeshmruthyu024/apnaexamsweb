89ed805bfd2b1e019ffe06c4b12c03cea4ce77cc
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2025-11-19 13:10:54 +0530

    added subheader

---
+++ src/components/common/SubHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './SubHeader.module.css'
+
+
+const SubHeader = () => {
+  return (
+    <div className={styles.subheader}>
+      <div className="container">
+        <h2 className='text-center text-white py-1'>Online Exams Write More Live Exams
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default SubHeader
